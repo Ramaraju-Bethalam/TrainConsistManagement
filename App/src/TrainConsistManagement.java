d4804bce1ef3480a6e37c619f9328a00f8857ab9
Ramaraju-Bethalam <ramaraju.bethalam24@gmail.com>
2026-04-02 14:52:01 +0530

    Preserve insertion order using Linked hash set

--- App/src/TrainConsistManagement.java
+++ App/src/TrainConsistManagement.java
@@ -5,7 +5,6 @@ public class TrainConsistManagement {
 
     public static void main(String[] args) {
 
-<<<<<<< feature/UC5
         System.out.println("=====================================");
         System.out.println("=== Train Consist Management App ===");
         System.out.println("=====================================\n");
@@ -21,28 +20,3 @@ public class TrainConsistManagement {
 
         System.out.println("Final Train Formation (No duplicates, ordered):");
         System.out.println(trainFormation);
-
-        System.out.println("\nTotal bogies: " + trainFormation.size());
-
-        System.out.println("\nProgram execution continues...");
-=======
-        System.out.println("=======================================");
-        System.out.println(" UC6 - Map Bogie to Capacity (HashMap) ");
-        System.out.println("=======================================\n");
-
-        Map<String, Integer> capacityMap = new HashMap<>();
-
-        capacityMap.put("First Class", 24);
-        capacityMap.put("Cargo", 120);
-        capacityMap.put("Sleeper", 72);
-        capacityMap.put("AC Chair", 56);
-
-        System.out.println("Bogie Capacity Details:\n");
-        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
-
-        System.out.println("\nUC6 bogie-capacity mapping completed...");
->>>>>>> main
-    }
-}
