f4cfd90db3aced77b500c2b7576a8a41e45b44fe
Ramaraju-Bethalam <ramaraju.bethalam24@gmail.com>
2026-04-02 14:19:51 +0530

    Map Bogie to capacity

--- App/src/TrainConsistManagement.java
+++ App/src/TrainConsistManagement.java
@@ -9,16 +9,13 @@ public class TrainConsistManagement {
         System.out.println(" UC6 - Map Bogie to Capacity (HashMap) ");
         System.out.println("=======================================\n");
 
-        // HashMap stores data in key -> value format
         Map<String, Integer> capacityMap = new HashMap<>();
 
-        // --- Insert bogie capacities ----
         capacityMap.put("First Class", 24);
         capacityMap.put("Cargo", 120);
         capacityMap.put("Sleeper", 72);
         capacityMap.put("AC Chair", 56);
 
-        // Display bogie capacity information
         System.out.println("Bogie Capacity Details:\n");
         for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
